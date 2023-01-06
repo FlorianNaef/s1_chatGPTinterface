e9c2b0270c3de66ca9090ec3d3dbda78fb405351
FlorianNaef <florian.naef@protonmail.com>
2023-01-06 13:06:47 +0100

    v1

---
+++ v1/main.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = "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"
+
+def chatbot_response(message):
+  model_engine = "text-davinci-002"
+  prompt = (f"{message}")
+
+  completions = openai.Completion.create(
+      engine=model_engine,
+      prompt=prompt,
+      max_tokens=1024,
+      n=1,
+      stop=None,
+      temperature=0.5,
+  )
+
+  message = completions.choices[0].text
+  return message.strip()
+
+while True:
+  message = input("You: ")
+  response = chatbot_response(message)
+  print("Assistant: ", response)
